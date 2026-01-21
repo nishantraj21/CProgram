ff25826ee5ac2dc85b374ca371f20ab5700ac693
Nishant Raj <113793557+nishantraj21@users.noreply.github.com>
2026-01-21 08:39:11 +0000

    recursion.c file added

---
+++ recursion.c
@@ -0,0 +1,11 @@
+// When fn call itself problem eg;
+// Tower of hanoi, DFS of graph, InOrder, PreOrder, PostOrder
+
+// When recursive fn calling itself one time then its linear recursion otherwise ,
+// if it calling itself more than one time then its tree recursion.
+
+// Space complexity of recursion is O(n)
+// Recursion       ||   Time complexity
+// Tail recursion  --> O(n)
+// Head recursion  --> O(n)
+// Tree recursion  --> O(2^n)
