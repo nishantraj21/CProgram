09cc0adf800ca03f0751b38e15a906f1e9c247fd
Nishant Raj <113793557+nishantraj21@users.noreply.github.com>
2026-01-21 09:01:22 +0000

    updated code

--- recursion.c
+++ recursion.c
@@ -9,3 +9,23 @@
 // Tail recursion  --> O(n)
 // Head recursion  --> O(n)
 // Tree recursion  --> O(2^n)
+#include<stdio.h>
+int f(int n)
+{
+    static int r = 0;
+    if(n <= 0) return 1;
+    if(n>3)
+    {
+        r = n;
+        return f(n-2)+2;
+    }
+    return f(n-1)+r;
+}
+
+int main()
+{
+    int d = f(5); // The output will be 18 because in fn f() r is static so its value will share all subsequence calls without change
+    // If r is not static then what will be 3.
+    printf("%d",d);
+    return 0;
+}
